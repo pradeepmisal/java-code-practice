530690423994a2f87d043555fe2fb3f4b091c966
pradeep misal <144873499+pradeepmisal@users.noreply.github.com>
2024-07-22 21:10:39 +0530

    "Replaced unfinished touppercase method with compress method implementation and updated main method to test compress function."

---
+++ Oop.java/Q1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Q1{
+    public static void main(String[] args) {
+        String str= "Hello";
+
+        for(int i=str.length()-1; i>=0; i--){
+            System.out.println(str.charAt(i));
+        }
+    }
+}
---
+++ Oop.java/substring.java
@@ -0,0 +1,32 @@
+public class substring {
+   
+    // public static void main(String[] args) {
+    //     String fruits[]={"mango", "banana", "apple"};
+    //     String largest = fruits[0];
+    //     for (int i=1; i<fruits.length; i++){
+    //         if(largest.compareTo(fruits[i]) < 0 ){
+    //             largest = fruits[i];
+    //         }
+    //     }
+    //     System.out.println(largest);
+    // // }/
+    // public static void main(String[] args) {
+    //     StringBuilder Sb= new StringBuilder("");
+    //     for(char ch='a'; ch<'m'; ch++){
+    //         Sb.append(ch);
+    //     }
+    //     System.out.println(Sb);
+
+    // }
+    public static String touppercase(String str){
+        StringBuilder sb= new StringBuilder("");
+        char ch =Character.toUpperCase(str.charAt(0));
+        for (int i=1; i<)
+
+    }
+    public static void main(String[] args) {
+        String str="hello i am pradeep";
+        
+    }
+    
+}
