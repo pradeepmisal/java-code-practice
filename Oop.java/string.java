987fa2f34db0556c1ccc4b0996d4c6d56cfc05df
pradeep misal <144873499+pradeepmisal@users.noreply.github.com>
2024-07-23 18:24:27 +0530

    Update string.java

--- Oop.java/string.java
+++ Oop.java/string.java
@@ -15,7 +15,7 @@ public class string {
         if (isPalindrome) {
             System.out.println("The string is a palindrome.");
         } else {
-            System.out.println("The string is  a palindrome.");
+            System.out.println("The string is not a palindrome.");
         }
     }
 }
